61b53932d0f883c4797b868e1ccbf3d468a61e52
Brian Mendes <brian.mendes.f@gmail.com>
2024-08-15 09:31:21 -0300

    (end) lesson with solution

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+	data: function () {
+		return {
+			product: "Socks",
+			description: "Comfortable and stylish high-calf socks made of soft wool. These socks come in vibrant shades of green and blue, adding a pop of color to your everyday wear."
+		};
+	},
+});
