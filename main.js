6aadaffd34d06dac47bc87db0c7f9185c628a279
Brian Mendes <brian.mendes.f@gmail.com>
2024-08-30 16:13:56 -0300

    (end) finished code challanges

--- main.js
+++ main.js
@@ -4,7 +4,32 @@ const app = Vue.createApp({
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
             inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
-        }
+            details: ['50% cotton', '30% wool', '20% polyester'],
+            variants: [
+                {
+                    id: 2234, color: 'green'
+                },
+                {
+                    id: 2235, color: 'blue'
+                }
+            ],
+            sizes: [
+                {
+                    id: 1, size: 'P'
+                },
+                {
+                    id: 2, size: 'M'
+                },
+                {
+                    id: 3, size: 'G'
+                },
+                {
+                    id: 4, size: 'GG'
+                },
+                {
+                    id: 5, size: 'XG'
+                }
+            ] 
+        };
     }
-})
+});
