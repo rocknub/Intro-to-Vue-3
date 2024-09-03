45825f969db7878806e1263d05810b4dffba2048
Brian Mendes <brian.mendes.f@gmail.com>
2024-09-03 08:52:39 -0300

    (end) challlange code finished

--- main.js
+++ main.js
@@ -1,15 +1,31 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart:0,
-            product: 'Socks',
-            image: './assets/images/socks_blue.jpg',
+            cart: 0,
+            product: "Socks",
+            image: "./assets/images/socks_blue.jpg",
             inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester'],
+            details: ["50% cotton", "30% wool", "20% polyester"],
             variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+                { id: 2234, color: "green", image: "./assets/images/socks_green.jpg" },
+                { id: 2235, color: "blue", image: "./assets/images/socks_blue.jpg" }
             ]
+        };
+    },
+
+    methods: {
+        addToCart() {
+            this.cart += 1;
+        },
+        removeFromCart() {
+            if (this.cart <= 0) {
+                return;
+            } else {
+                this.cart -= 1;
+            }
+        },
+        updateImage(variantImage) {
+            this.image = variantImage;
         }
     }
-})
+});
